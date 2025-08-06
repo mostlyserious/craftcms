4baebe5595040c714f83dda1ca578f5d32dc4511
Cornelius Ukena <cornelius.ukena@mostlyserious.io>
2025-08-06 10:53:08 -0500

    Fix typo in config check

--- config/element-api.php
+++ config/element-api.php
@@ -6,7 +6,7 @@ use Craft;
 
 return [
     'defaults' => [
-        'cache' => Craft::$app->config->cacheDuration !== 'dev'
+        'cache' => Craft::$app->config->env !== 'dev'
             ? Craft::$app->config->general->cacheDuration
             : false,
     ],
