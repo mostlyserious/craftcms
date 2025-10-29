d14031236f9efdadfe2949b62c758cb64bb06f47
Cornelius Ukena <cornelius.ukena@mostlyserious.io>
2025-10-29 14:45:32 -0500

    Convert regexp to endsWith call

--- utility/vite-plugin-svgo.js
+++ utility/vite-plugin-svgo.js
@@ -8,7 +8,7 @@ export default (options = {}) => ({
     name: 'vite-plugin-svgo',
     generateBundle(_, bundler) {
         for (const [ path, asset ] of Object.entries(bundler)) {
-            if ((/\.svg$/).test(path)) {
+            if (path.endsWith('.svg')) {
                 const { data } = optimize(asset.source.toString(), { ...options, path })
 
                 asset.source = Buffer.from(data, 'utf-8')
