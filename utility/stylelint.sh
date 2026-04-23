ba597977c0f19057e2a37347c50446d8e553c503
Cornelius Ukena <cornelius.ukena@mostlyserious.io>
2026-04-23 11:26:27 -0500

    Format editor configs and clean up utility files

--- utility/stylelint.sh
+++ utility/stylelint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ ! -f "${PWD}/stylelint.config.js" ]; then
-    printf "export default {\n    extends: 'stylelint-config-hudochenkov/order',\n}\n" > "${PWD}/stylelint.config.js"
-    bun add stylelint-config-hudochenkov --dev &>/dev/null
-    sleep 1
-fi
-
 if [ "$1" = "html" ]; then
     ./node_modules/.bin/stylelint --fix --stdin --custom-syntax=postcss-html
 else
